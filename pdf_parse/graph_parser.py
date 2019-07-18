dca3bed5b6f9c479240e625ecb3ab6f04453fb51
spyder <spyder@DJ-Mac.local>
2019-07-18 15:41:32 -0400

    added Graph/Axis classes

--- pdf_parse/graph_parser.py
+++ pdf_parse/graph_parser.py
@@ -1,11 +1,13 @@
 import math
 import skimage
 from skimage import data, io
+import numpy as np
+import matplotlib.pyplot as plt
 
 #!usr/bin/python3.7.3
 ### Graph_Parser Class ###
 # Overview: Input graph, select points to mark axis, then interpolate data
-#
+#   Requirements: sci-kit image, matplotlib, numpy, math
 #
 
 #v1 - v2
@@ -30,16 +32,27 @@ class Axis(object):
     def __init__(self, start:tuple, end:tuple, values:tuple=None):
         self.loc = (start, end);
         if(values):
-            self.val = values
+            self.val = sorted(values)
+            
+    # acccessors        
+    def get_ycoord(self):
+        return sorted((self.loc[0][0], self.loc[1][0]))
 
+    def get_xcoord(self):
+        return sorted((self.loc[0][1], self.loc[1][1]))
+
+    def get_range(self):
+        return self.val
+
+    # mutator
     def set_value(self, values:tuple):
         self.val = values;
     
     def pt_approx(self, pt:tuple):
+        # Approximate value at given point relative to axis
         v1   = vsub(pt         , self.loc[0])
         v2   = vsub(self.loc[1], self.loc[0])
         vlen = magn(proj(v1, v2))
-
         return vlen/magn(v2)*(self.val[1]-self.val[0])+self.val[0]
 
 
@@ -47,7 +60,51 @@ class Graph_Parser(object):
     # vax=vertical axis, hax=horizontal Axis
     def __init__(self, vp1:tuple , vp2:tuple, 
                        hp1:tuple , hp2:tuple,
-                       vval:tuple, hval:tuple):
+                       vval:tuple, hval:tuple,
+                       pt:tuple = None,
+                       path:str = None):
+        self.vax      = Axis(vp2, vp1, vval)
+        self.hax      = Axis(hp1, hp2, hval)
+        
+        # Optional parameters
+        if(pt):self.color_pt = pt;        
+        if(path):self.img = np.array(io.imread(path))
+        if(pt and path): self.set_color()
+        
+    #mutators
+    def set_image(self, path:str = ''):
+        self.img = np.array(io.imread(path))
+
+    def set_vax(self, vp1:tuple, vp2:tuple, vval:tuple):
         self.vax = Axis(vp1, vp2, vval)
+
+    def set_hax(self, hp1:tuple, hp2:tuple, hval:tuple):
         self.hax = Axis(hp1, hp2, hval)
 
+    def set_color(self, pt=None):
+        if(pt):
+            self.color = self.img[pt[0], pt[1]]
+        else:
+            self.color = self.img[self.color_pt[0], self.color_pt[1]]
+    
+    # Approximate time-series      
+    def get_pts(self, step:int = 1, mode:str='color'):
+        if(mode == 'color'):
+            xax = self.hax.get_xcoord()
+            yax = self.vax.get_ycoord()
+            pts = []
+            for t in range(xax[0]+2, xax[1]-2, step):
+                vval = 0
+                tval = 0
+                c = 0
+                
+                for v in range(yax[0], yax[1]-3):
+                    # If color matches
+                    if(np.array_equal(self.img[v, t], self.color)):
+                        vval += self.vax.pt_approx((v, t))
+                        tval += self.hax.pt_approx((v, t))
+                        c += 1
+                        
+                if(c != 0): pts.append((tval/c, vval/c)) # Identify colors/average values found and append
+            
+            return pts
