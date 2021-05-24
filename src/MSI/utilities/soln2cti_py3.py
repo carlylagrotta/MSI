f10fea10041791dbf2a3188a386d3cca80e4484e
Mark Barbet <mcb2261@columbia.edu>
2021-05-24 15:14:30 -0400

    Added support to soln2cti_py3.py for new version of Cantera.  In new Cantera, falloff 'simple' was changed to 'Lindemann'.  Support added for both keywords.

--- src/MSI/utilities/soln2cti_py3.py
+++ src/MSI/utilities/soln2cti_py3.py
@@ -574,14 +574,14 @@ def write(solution,cwd='',file_name='',original_cti=''):
                 j = equation_object.falloff.parameters
                 #If optional Arrhenius data included:
                  
-                if equation_object.falloff.type!='Simple':
+                if equation_object.falloff.type!='Simple' and equation_object.fallof.type!='Lindemann':
                     try:
                         falloff_str = build_falloff(equation_object,j)
                         #print(falloff_str)
                         f.write(falloff_str)
                     except IndexError:
                         f.write('\n           )\n\n')
-                elif equation_object.falloff.type=='Simple':
+                elif equation_object.falloff.type=='Simple' or equation_object.type=='Lindemann':
                     f.write('\n           )\n\n')
             if equation_type=='PlogReaction':
                 f.write('#  Reaction '+str(m)+'\n')
