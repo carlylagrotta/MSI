5b255c8008e7dbaa3f1ad270cc6ae4596ee14336
carlylagrotta <cel2169@columbia.edu>
2021-06-10 16:14:28 -0400

    delteing files;

--- src/MSI/utilities/plotting_trash.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-class Plotting_trash(object):
-    def __init__(self):
-        self.x=2
-    def plot_trash(self):
-        plt.plot([1,2,4,5],[1,2,3,4,5])
